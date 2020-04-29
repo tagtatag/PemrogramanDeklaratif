8186796e74b634f4b68aa982be965ead12b1281a
tagtatag <57257894+tagtatag@users.noreply.github.com>
2020-04-30 06:45:53 +0700

    Add files via upload

---
+++ imp_modul_2/puzzle.py
@@ -0,0 +1,23 @@
+
+from pyswip.prolog import Prolog
+
+
+
+def main():
+    prolog = Prolog()
+    prolog.consult("prolog_file/puzzle1.pl")
+    for soln in prolog.query("solve(B)."):
+        #B = eval(soln["B"])
+        B = soln["B"]
+        # [NW,N,NE,W,E,SW,S,SE]
+        print("%d %d %d" % tuple(B[:3]))
+        print("%d   %d"   % tuple(B[3:5]))
+        print("%d %d %d" % tuple(B[5:]))
+
+        cont = input("Press 'n' to finish: ")
+        if cont.lower() == "n": break
+
+
+if __name__ == "__main__":
+    main()
+()
