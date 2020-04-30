6c87bad516e214d727e6222069b0440ffe4a5859
NandhaMS <nandhamustikasari.1805356@students.um.ac.id>
2020-04-30 08:09:28 +0700

    Update coins.py

--- imp_modul_3/coins.py
+++ imp_modul_3/coins.py
@@ -32,7 +32,7 @@ from pyswip.prolog import Prolog
 def main():
     prolog = Prolog()
     prolog.consult("prolog_file/coins.pl")
-    count = int(input("How many coins (default: 100)? ") or 100)
+    count = int(input("Berapa jumlah koin(default: 100)? ") or 100)
     total = int(input("What should be the total (default: 500)? ") or 500)
     for i, soln in enumerate(prolog.query("coins(S, %d, %d)." % (count,total))):
         # [1,5,10,50,100]
