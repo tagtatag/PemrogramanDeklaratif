a0d1649e73912a1dd46846c2516107a327abdccb
NandhaMS <nandhamustikasari.1805356@students.um.ac.id>
2020-04-30 08:40:50 +0700

    Update coins.py

--- imp_modul_3/coins.py
+++ imp_modul_3/coins.py
@@ -33,7 +33,7 @@ def main():
     prolog = Prolog()
     prolog.consult("prolog_file/coins.pl")
     count = int(input("Berapa jumlah koin(default: 100)? ") or 100)
-    total = int(input("What should be the total (default: 500)? ") or 500)
+    total = int(input("Jumlah target total (default: 500)? ") or 500)
     for i, soln in enumerate(prolog.query("coins(S, %d, %d)." % (count,total))):
         # [1,5,10,50,100]
         S = zip(soln["S"], [1, 5, 10, 50, 100])
