bc101abb63c4518460a11391f27684dcf923e082
NandhaMS <64564337+NandhaMS@users.noreply.github.com>
2020-04-30 16:41:21 +0700

    Final Progress

--- imp_modul_3/coins.py
+++ imp_modul_3/coins.py
@@ -1,48 +1,39 @@
-# -*- coding: utf-8 -*-
+#import library pyswip
+from pyswip.prolog import Prolog
 
+#pyswip telah menyediakan dan telah menghubungkan python dengan bahasa prolog,
+#sehingga tidak diperlukan file prolog eksternal
 
-# pyswip -- Python SWI-Prolog bridge
-# Copyright (c) 2007-2012 Yüce Tekol
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-
-# 100 coins must sum to $5.00
-
-
-from pyswip.prolog import Prolog   #import library pyswip
-
-
+#fungsi utama
 def main():
     prolog = Prolog()
-    prolog.consult("prolog_file/coins.pl")     #membuka file coins.pl
-    count = int(input("Berapa jumlah koin(default: 100)? ") or 100)     #menginputkan nilai count
-    total = int(input("Jumlah target total (default: 500)? ") or 500)   #menginputkan nilai total
+    
+    #membuka file coins.pl
+    prolog.consult("prolog_file/coins.pl")
+    
+    #mendeklarasi nilai count dan total
+    #menginputkan nilai count dan nilai total berupa angka
+    count = int(input("Berapa jumlah koin(default: 100)? ") or 100)
+    total = int(input("Jumlah target total (default: 500)? ") or 500)
+    
     #melakukan perulangan dengan menampilkan nilai yang ada di dalam list
+    #enumerate berfungsi untuk mengembalikan sebuah objek iterable yang setiap itemnya memiliki indeks
+    #memanggil fungsi coins pada prolog
     for i, soln in enumerate(prolog.query("coins(S, %d, %d)." % (count,total))):
         # [1,5,10,50,100]
         S = zip(soln["S"], [1, 5, 10, 50, 100])
-        print(i),
+        print(i), #cetak nilai
+        
+        #melakukan perulangan untuk menampilkan angka perkalian
         for c, v in S:
-            print("%dx%d" % (c,v)),    #menampilkan angka perkalian
-        print
-    list(prolog.query("coins(S, %d, %d)." % (count,total)))   #menampilkan seluruh list
+            print("%dx%d" % (c,v)),  #cetak perkalian
+        print  #cetak hasil perulangan
+        
+    #fungsi list yaitu mengembalikan list berisi anggota-anggota dari objek yang menjadi argumennya
+    #jika argumennya kosong, maka fungsi ini akan mengembalikan list kosong
+    #jika argumennya berisikan data, maka yang digunakan adalah data-data yang digunakan dari list tersebut
+    #hasil akhirnya akan menampilkan perhitungan perkalian dari jumlah nilai yang diinputkan
+    list(prolog.query("coins(S, %d, %d)." % (count,total)))
 
 
 if __name__ == "__main__":
