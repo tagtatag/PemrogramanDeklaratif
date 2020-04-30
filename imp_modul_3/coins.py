2d97baec9816b1df63fc91b962ad1175381123b1
MRRukantala <rafly.hernandes@gmail.com>
2020-04-30 12:34:21 +0700

    Update coins.py

--- imp_modul_3/coins.py
+++ imp_modul_3/coins.py
@@ -35,14 +35,14 @@ def main():
     count = int(input("Berapa jumlah koin(default: 100)? ") or 100)     #menginputkan nilai count
     total = int(input("Jumlah target total (default: 500)? ") or 500)   #menginputkan nilai total
     #melakukan perulangan dengan menampilkan nilai yang ada di dalam list
-    for i, soln in enumerate(prolog.query("Koin(S, %d, %d)." % (count,total))):
+    for i, soln in enumerate(prolog.query("coins(S, %d, %d)." % (count,total))):
         # [1,5,10,50,100]
         S = zip(soln["S"], [1, 5, 10, 50, 100])
         print(i),
         for c, v in S:
             print("%dx%d" % (c,v)),    #menampilkan angka perkalian
         print
-    list(prolog.query("Koin(S, %d, %d)." % (count,total)))   #menampilkan seluruh list
+    list(prolog.query("coins(S, %d, %d)." % (count,total)))   #menampilkan seluruh list
 
 
 if __name__ == "__main__":
